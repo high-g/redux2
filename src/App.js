1a326995c074f7de48088f808c9b572b88f33206
tani <tani@1-10.com>
2021-07-12 15:42:59 +0900

    Update useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
+import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addTodoAction } from './reducks/todos/actions'
 
 function App() {
+  const [text, setText] = useState('')
   const dispatch = useDispatch()
-  const state = useSelector((state) => state)
-
-  console.log('state', state)
+  const state = useSelector((state) => state.todos)
 
   return (
     <div className="App">
       <h1>Todoリスト</h1>
+      <input
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value)
+        }}
+      />
       <button
         onClick={() => {
-          dispatch(addTodoAction({ text: 'Add' }))
+          dispatch(addTodoAction({ text }))
+          setText('')
         }}
       >
         Add
       </button>
       <ul>
-        {state.todos.todos.list.map((todo) => {
+        {state.todos.list.map((todo, index) => {
           console.log('todo', todo)
-          return <li>{todo.text}</li>
+          return <li key={index}>{todo.text}</li>
         })}
       </ul>
     </div>
